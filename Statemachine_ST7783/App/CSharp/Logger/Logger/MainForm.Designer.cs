e69d3917c422a86db99cef5cb9084bf52d2d9e82
volthouse <lucasgaertner@arcor.de>
2014-05-20 07:28:37 +0200

    Add CSharp Demo.

---
+++ Statemachine_ST7783/App/CSharp/Logger/Logger/MainForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Logger {
+	partial class Main {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.setDateTimeButton = new System.Windows.Forms.Button();
+			this.portComboBox = new System.Windows.Forms.ComboBox();
+			this.resetButton = new System.Windows.Forms.Button();
+			this.portComboLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// setDateTimeButton
+			// 
+			this.setDateTimeButton.Location = new System.Drawing.Point(79, 79);
+			this.setDateTimeButton.Name = "setDateTimeButton";
+			this.setDateTimeButton.Size = new System.Drawing.Size(127, 23);
+			this.setDateTimeButton.TabIndex = 1;
+			this.setDateTimeButton.Text = "Set DateTime";
+			this.setDateTimeButton.UseVisualStyleBackColor = true;
+			this.setDateTimeButton.Click += new System.EventHandler(this.setDateTimeButton_Click);
+			// 
+			// portComboBox
+			// 
+			this.portComboBox.FormattingEnabled = true;
+			this.portComboBox.Location = new System.Drawing.Point(122, 28);
+			this.portComboBox.Name = "portComboBox";
+			this.portComboBox.Size = new System.Drawing.Size(84, 21);
+			this.portComboBox.TabIndex = 0;
+			// 
+			// resetButton
+			// 
+			this.resetButton.Location = new System.Drawing.Point(79, 108);
+			this.resetButton.Name = "resetButton";
+			this.resetButton.Size = new System.Drawing.Size(127, 23);
+			this.resetButton.TabIndex = 2;
+			this.resetButton.Text = "Reset";
+			this.resetButton.UseVisualStyleBackColor = true;
+			this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+			// 
+			// portComboLabel
+			// 
+			this.portComboLabel.AutoSize = true;
+			this.portComboLabel.Location = new System.Drawing.Point(76, 31);
+			this.portComboLabel.Name = "portComboLabel";
+			this.portComboLabel.Size = new System.Drawing.Size(29, 13);
+			this.portComboLabel.TabIndex = 3;
+			this.portComboLabel.Text = "Port:";
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 143);
+			this.Controls.Add(this.portComboLabel);
+			this.Controls.Add(this.resetButton);
+			this.Controls.Add(this.portComboBox);
+			this.Controls.Add(this.setDateTimeButton);
+			this.Name = "Main";
+			this.Text = "Logger";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button setDateTimeButton;
+		private System.Windows.Forms.ComboBox portComboBox;
+		private System.Windows.Forms.Button resetButton;
+		private System.Windows.Forms.Label portComboLabel;
+	}
+}
+
---
+++ Statemachine_ST7783/App/CSharp/Logger/Logger/MainForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Logger {
+	public partial class Main : Form {
+		
+		public Main() {
+			InitializeComponent();
+
+			portComboBox.Items.AddRange(SerialPort.GetPortNames());
+
+		}
+
+		private bool tryGetPortName(out string portName) {
+			portName = portComboBox.Text;
+
+			if (portName.Length == 0) {
+				MessageBox.Show("select com port", "Serial Port");
+				return false;
+			}
+
+			return true;
+		}
+
+		private byte getCheckSum(byte[] buffer, int count) {
+			byte sum = 0;
+			for (int i = 0; i < count; i++) {
+				sum += buffer[i];
+			}
+			return sum;
+		}
+
+		private void setDateTimeButton_Click(object sender, EventArgs e) {
+			string portName;
+			if(tryGetPortName(out portName)) {
+				byte[] time = { /*0:Signature*/ 0xCC, 0x33, 0x55, 0xAA, /*4:time*/ 0, /*5:hour*/  0, /*6:minute*/ 0, /*7:second*/ 0, /*8*/     0, /*9*/ 0 };
+				byte[] date = { /*0:Signature*/ 0xCC, 0x33, 0x55, 0xAA, /*4:date*/ 1, /*5:year*/ 19, /*6:year*/  70, /*7:month*/  0, /*8:day*/ 0, /*9*/ 0 };
+
+				time[5] = (byte)DateTime.Now.Hour;
+				time[6] = (byte)DateTime.Now.Minute;
+				time[7] = (byte)DateTime.Now.Second;
+				time[9] = getCheckSum(time, 8);
+
+				SerialPort serial = new SerialPort(portName, 9600);
+				serial.DtrEnable = false;
+				serial.Open();
+				serial.Write(time, 0, 9);
+				serial.Write(date, 0, 9);
+				serial.Close();
+			}
+		}
+
+		private void resetButton_Click(object sender, EventArgs e) {
+			string portName;
+			if (tryGetPortName(out portName)) {
+				SerialPort serial = new SerialPort(portName, 9600);
+				serial.DtrEnable = true;
+				serial.Open();
+				serial.Close();
+			}
+		}
+	}
+}
