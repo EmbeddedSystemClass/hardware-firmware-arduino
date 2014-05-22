aaa46ccf01882ee6cbd18609b7cd70f235552916
volthouse <lucasgaertner@arcor.de>
2014-05-22 15:59:30 +0200

    Add reporting.

--- Statemachine_ST7783/App/CSharp/Logger/Logger/MainForm.Designer.cs
+++ Statemachine_ST7783/App/CSharp/Logger/Logger/MainForm.Designer.cs
@@ -23,15 +23,29 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+			this.BindingSource = new System.Windows.Forms.BindingSource(this.components);
 			this.setDateTimeButton = new System.Windows.Forms.Button();
 			this.portComboBox = new System.Windows.Forms.ComboBox();
 			this.resetButton = new System.Windows.Forms.Button();
 			this.portComboLabel = new System.Windows.Forms.Label();
+			this.getTemperatureButton = new System.Windows.Forms.Button();
+			this.tempLabel = new System.Windows.Forms.Label();
+			this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+			this.getTemperatureLogButton = new System.Windows.Forms.Button();
+			this.temperatureItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.BindingSource)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.temperatureItemBindingSource)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// BindingSource
+			// 
+			this.BindingSource.DataSource = typeof(Logger.TemperatureItem);
 			// 
 			// setDateTimeButton
 			// 
-			this.setDateTimeButton.Location = new System.Drawing.Point(79, 79);
+			this.setDateTimeButton.Location = new System.Drawing.Point(12, 36);
 			this.setDateTimeButton.Name = "setDateTimeButton";
 			this.setDateTimeButton.Size = new System.Drawing.Size(127, 23);
 			this.setDateTimeButton.TabIndex = 1;
@@ -42,14 +56,15 @@
 			// portComboBox
 			// 
 			this.portComboBox.FormattingEnabled = true;
-			this.portComboBox.Location = new System.Drawing.Point(122, 28);
+			this.portComboBox.Location = new System.Drawing.Point(55, 9);
 			this.portComboBox.Name = "portComboBox";
 			this.portComboBox.Size = new System.Drawing.Size(84, 21);
 			this.portComboBox.TabIndex = 0;
+			this.portComboBox.SelectedIndexChanged += new System.EventHandler(this.portComboBox_SelectedIndexChanged);
 			// 
 			// resetButton
 			// 
-			this.resetButton.Location = new System.Drawing.Point(79, 108);
+			this.resetButton.Location = new System.Drawing.Point(12, 365);
 			this.resetButton.Name = "resetButton";
 			this.resetButton.Size = new System.Drawing.Size(127, 23);
 			this.resetButton.TabIndex = 2;
@@ -60,23 +75,74 @@
 			// portComboLabel
 			// 
 			this.portComboLabel.AutoSize = true;
-			this.portComboLabel.Location = new System.Drawing.Point(76, 31);
+			this.portComboLabel.Location = new System.Drawing.Point(9, 12);
 			this.portComboLabel.Name = "portComboLabel";
 			this.portComboLabel.Size = new System.Drawing.Size(29, 13);
 			this.portComboLabel.TabIndex = 3;
 			this.portComboLabel.Text = "Port:";
 			// 
+			// getTemperatureButton
+			// 
+			this.getTemperatureButton.Location = new System.Drawing.Point(12, 94);
+			this.getTemperatureButton.Name = "getTemperatureButton";
+			this.getTemperatureButton.Size = new System.Drawing.Size(127, 23);
+			this.getTemperatureButton.TabIndex = 4;
+			this.getTemperatureButton.Text = "Get Temp";
+			this.getTemperatureButton.UseVisualStyleBackColor = true;
+			this.getTemperatureButton.Click += new System.EventHandler(this.getTemperatureButton_Click);
+			// 
+			// tempLabel
+			// 
+			this.tempLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.tempLabel.Location = new System.Drawing.Point(12, 120);
+			this.tempLabel.Name = "tempLabel";
+			this.tempLabel.Size = new System.Drawing.Size(127, 23);
+			this.tempLabel.TabIndex = 5;
+			this.tempLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// reportViewer1
+			// 
+			reportDataSource1.Name = "TemperatureItem";
+			reportDataSource1.Value = this.BindingSource;
+			this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+			this.reportViewer1.LocalReport.ReportEmbeddedResource = "Logger.TemperatureReport.rdlc";
+			this.reportViewer1.Location = new System.Drawing.Point(145, 9);
+			this.reportViewer1.Name = "reportViewer1";
+			this.reportViewer1.ShowToolBar = false;
+			this.reportViewer1.Size = new System.Drawing.Size(543, 379);
+			this.reportViewer1.TabIndex = 6;
+			// 
+			// getTemperatureLogButton
+			// 
+			this.getTemperatureLogButton.Location = new System.Drawing.Point(12, 65);
+			this.getTemperatureLogButton.Name = "getTemperatureLogButton";
+			this.getTemperatureLogButton.Size = new System.Drawing.Size(127, 23);
+			this.getTemperatureLogButton.TabIndex = 7;
+			this.getTemperatureLogButton.Text = "Get Log";
+			this.getTemperatureLogButton.UseVisualStyleBackColor = true;
+			this.getTemperatureLogButton.Click += new System.EventHandler(this.getTemperatureLogButton_Click);
+			// 
+			// temperatureItemBindingSource
+			// 
+			this.temperatureItemBindingSource.DataSource = typeof(Logger.TemperatureItem);
+			// 
 			// Main
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 143);
+			this.ClientSize = new System.Drawing.Size(697, 400);
+			this.Controls.Add(this.getTemperatureLogButton);
+			this.Controls.Add(this.reportViewer1);
+			this.Controls.Add(this.tempLabel);
+			this.Controls.Add(this.getTemperatureButton);
 			this.Controls.Add(this.portComboLabel);
 			this.Controls.Add(this.resetButton);
 			this.Controls.Add(this.portComboBox);
 			this.Controls.Add(this.setDateTimeButton);
 			this.Name = "Main";
 			this.Text = "Logger";
+			((System.ComponentModel.ISupportInitialize)(this.BindingSource)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.temperatureItemBindingSource)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -88,6 +154,12 @@
 		private System.Windows.Forms.ComboBox portComboBox;
 		private System.Windows.Forms.Button resetButton;
 		private System.Windows.Forms.Label portComboLabel;
+		private System.Windows.Forms.Button getTemperatureButton;
+		private System.Windows.Forms.Label tempLabel;
+		private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+		private System.Windows.Forms.BindingSource temperatureItemBindingSource;
+		private System.Windows.Forms.BindingSource BindingSource;
+		private System.Windows.Forms.Button getTemperatureLogButton;		
 	}
 }
 
--- Statemachine_ST7783/App/CSharp/Logger/Logger/MainForm.cs
+++ Statemachine_ST7783/App/CSharp/Logger/Logger/MainForm.cs
@@ -9,13 +9,15 @@ using System.Windows.Forms;
 using System.IO.Ports;
 
 namespace Logger {
+
 	public partial class Main : Form {
-		
+
 		public Main() {
 			InitializeComponent();
 
 			portComboBox.Items.AddRange(SerialPort.GetPortNames());
 
+			updateUi();
 		}
 
 		private bool tryGetPortName(out string portName) {
@@ -29,9 +31,9 @@ namespace Logger {
 			return true;
 		}
 
-		private byte getCheckSum(byte[] buffer, int count) {
+		private byte getCheckSum(byte[] buffer, int offset, int count) {
 			byte sum = 0;
-			for (int i = 0; i < count; i++) {
+			for (int i = offset; i < offset + count; i++) {
 				sum += buffer[i];
 			}
 			return sum;
@@ -40,19 +42,35 @@ namespace Logger {
 		private void setDateTimeButton_Click(object sender, EventArgs e) {
 			string portName;
 			if(tryGetPortName(out portName)) {
-				byte[] time = { /*0:Signature*/ 0xCC, 0x33, 0x55, 0xAA, /*4:time*/ 0, /*5:hour*/  0, /*6:minute*/ 0, /*7:second*/ 0, /*8*/     0, /*9*/ 0 };
-				byte[] date = { /*0:Signature*/ 0xCC, 0x33, 0x55, 0xAA, /*4:date*/ 1, /*5:year*/ 19, /*6:year*/  70, /*7:month*/  0, /*8:day*/ 0, /*9*/ 0 };
+				// frame signature
+				byte[] signature = { 0xCC, 0x33, 0x55, 0xAA };
+				
+				// set time command
+				byte[] time = { /*0:set time cmd*/ 0, /*1:hh*/ 0, /*2:mm*/ 0, /*3:ss*/ 0, /*4:empty*/ 0, /*5:empty*/ 0, /*6:checksum*/ 0 };				
+				
+				time[1] = (byte)DateTime.Now.Hour;
+				time[2] = (byte)DateTime.Now.Minute;
+				time[3] = (byte)DateTime.Now.Second;				
+				time[6] = getCheckSum(time, 0, 6);
 
-				time[5] = (byte)DateTime.Now.Hour;
-				time[6] = (byte)DateTime.Now.Minute;
-				time[7] = (byte)DateTime.Now.Second;
-				time[9] = getCheckSum(time, 8);
+				// set date command
+				byte[] date = { /*0:set date cmd*/ 1, /*1:yy*/ 0, /*2:yy*/ 0, /*3:mm*/ 0, /*4:dd   */ 0, /*5:empty*/ 0, /*6:Checksum*/ 0 };
+				date[1] = (byte)(DateTime.Now.Year / 1000);
+				date[2] = (byte)(DateTime.Now.Year % 1000) ;
+				date[3] = (byte)DateTime.Now.Month;
+				date[4] = (byte)DateTime.Now.Day;
+				date[6] = getCheckSum(time, 0, 6);
 
+				// serial communication
 				SerialPort serial = new SerialPort(portName, 9600);
 				serial.DtrEnable = false;
 				serial.Open();
-				serial.Write(time, 0, 9);
-				serial.Write(date, 0, 9);
+
+				serial.Write(signature, 0, 4);
+				serial.Write(time, 0, 7);
+				serial.Write(signature, 0, 4);
+				serial.Write(date, 0, 7);
+				
 				serial.Close();
 			}
 		}
@@ -66,5 +84,98 @@ namespace Logger {
 				serial.Close();
 			}
 		}
+
+		private void getTemperatureButton_Click(object sender, EventArgs e) {
+			string portName;
+			if (tryGetPortName(out portName)) {
+				// frame signature
+				byte[] signature = { 0xCC, 0x33, 0x55, 0xAA };
+				// get temperature command
+				byte[] getTemperature = { /*0:get temperature*/ 2, /*1:empty*/  0, /*2:empty*/ 0, /*3:empty*/ 0, /*4:empty*/ 0, /*5:empty*/ 0, /*6:Checksum*/ 2 };
+				
+				// serial communication
+				SerialPort serial = new SerialPort(portName, 9600);
+				serial.DtrEnable = false;
+				serial.Open();
+				serial.Write(signature, 0, 4);
+				serial.Write(getTemperature, 0, 7);
+
+				System.Threading.Thread.Sleep(100);
+				
+				// read temperature
+				for (int i = 0; i < 100; i++) {
+					if (serial.BytesToRead > 0) {
+						tempLabel.Text = serial.ReadChar().ToString();
+						break;
+					} else {
+						tempLabel.Text = "failed";
+					}
+				}
+				
+				serial.Close();
+			}
+		}
+		
+
+		private void getTemperatureLogButton_Click(object sender, EventArgs e) {
+			string portName;
+			if (tryGetPortName(out portName)) {
+				// frame signature
+				byte[] signature = { 0xCC, 0x33, 0x55, 0xAA };
+				// get temperature command
+				byte[] getTemperature = { /*0:get log*/ 3, /*1:empty*/  0, /*2:empty*/ 0, /*3:empty*/ 0, /*4:empty*/ 0, /*5:empty*/ 0, /*6:Checksum*/ 2 };
+
+				// serial communication
+				SerialPort serial = new SerialPort(portName, 9600);
+				serial.DtrEnable = false;
+				serial.Open();
+				serial.Write(signature, 0, 4);
+				serial.Write(getTemperature, 0, 7);
+
+				System.Threading.Thread.Sleep(100);
+
+				List<TemperatureItem> logItems = new List<TemperatureItem>();
+						// read temperature
+				for (int i = 0; i < 100; i++) {					
+					if (serial.BytesToRead > 0) {						
+						for (int n = 0; n < serial.BytesToRead; n++) {
+							logItems.Add(new TemperatureItem() { 
+								Id = logItems.Count + 1, Temperature = (byte)serial.ReadByte() }							
+							);
+						}
+
+						if (logItems.Count >= 24)
+							break;
+					} else {
+						tempLabel.Text = "failed";
+					}
+				}
+				
+				serial.Close();
+
+				this.BindingSource.DataSource = logItems;
+				reportViewer1.RefreshReport();
+			}
+		}
+
+		private void portComboBox_SelectedIndexChanged(object sender, EventArgs e) {
+			updateUi();
+		}
+
+		private void updateUi() {
+			bool enabled = portComboBox.SelectedIndex >= 0;
+			setDateTimeButton.Enabled = enabled;
+			getTemperatureLogButton.Enabled = enabled;
+			getTemperatureButton.Enabled = enabled;
+			setDateTimeButton.Enabled = enabled;
+			resetButton.Enabled = false;
+		}
 	}
+
+	public class TemperatureItem {
+		public TemperatureItem() {
+		}
+		public int Id { get; set; }
+		public byte Temperature { get; set; }
+	}	
 }
