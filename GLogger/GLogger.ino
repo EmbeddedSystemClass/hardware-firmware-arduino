db5d8b3124c638e6a67c449103111afcb5c2cdd5
volthouse <lucasgaertner@arcor.de>
2015-03-30 16:03:45 +0200

    switch to fat16.

--- GLogger/GLogger.ino
+++ GLogger/GLogger.ino
@@ -1,26 +1,28 @@
 /*
   SD G-Force datalogger
  
- The circuit:
- * ADXL345 Accelerometer sensor via I2C
- 
- * SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 10
- 
- This example code is in the public domain.
+   The circuit:
+   * ADXL345 Accelerometer sensor via I2C
+   
+   * SD card attached to SPI bus as follows:
+   ** MOSI - pin 11
+   ** MISO - pin 12
+   ** CLK - pin 13
+   ** CS - pin 10
+   
+   This example code is in the public domain.
 
  */
-
+ 
 #include <SPI.h>
-#include <SD.h>
+#include <SdFat.h>
+//#include <Fat16util.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
 
-//#define DEBUG 0
+SdFat card;
+SdFile f;
 
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
@@ -33,129 +35,26 @@ const long debounceDelay = 50;    // the debounce time; increase if the output f
 int ledState = HIGH;        // the current state of the output pin
 int buttonState;            // the current reading from the input pin
 int lastButtonState = LOW;  // the previous reading from the input pin
+int logEnabled = 0;         // log state
 long lastDebounceTime = 0;  // the last time the output pin was toggled
 long lastLogTime = 0;       // the last time data logged
 
-int logEnabled = 0;         // log state  
+//void enableSD(void){
+//  DDRB = DDRB & 0xDF; //pinMode(CS_LCD,INPUT);
+//  DDRB = DDRB | 0x10; //pinMode(CS_SD,OUTPUT);
+//}
 
-File dataFile;              // data file log
-
-#ifdef DEBUG
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  accel.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");  
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
-}
-
-void displayDataRate(void)
-{
-  Serial.print  ("Data Rate:    "); 
-  
-  switch(accel.getDataRate())
-  {
-    case ADXL345_DATARATE_3200_HZ:
-      Serial.print  ("3200 "); 
-      break;
-    case ADXL345_DATARATE_1600_HZ:
-      Serial.print  ("1600 "); 
-      break;
-    case ADXL345_DATARATE_800_HZ:
-      Serial.print  ("800 "); 
-      break;
-    case ADXL345_DATARATE_400_HZ:
-      Serial.print  ("400 "); 
-      break;
-    case ADXL345_DATARATE_200_HZ:
-      Serial.print  ("200 "); 
-      break;
-    case ADXL345_DATARATE_100_HZ:
-      Serial.print  ("100 "); 
-      break;
-    case ADXL345_DATARATE_50_HZ:
-      Serial.print  ("50 "); 
-      break;
-    case ADXL345_DATARATE_25_HZ:
-      Serial.print  ("25 "); 
-      break;
-    case ADXL345_DATARATE_12_5_HZ:
-      Serial.print  ("12.5 "); 
-      break;
-    case ADXL345_DATARATE_6_25HZ:
-      Serial.print  ("6.25 "); 
-      break;
-    case ADXL345_DATARATE_3_13_HZ:
-      Serial.print  ("3.13 "); 
-      break;
-    case ADXL345_DATARATE_1_56_HZ:
-      Serial.print  ("1.56 "); 
-      break;
-    case ADXL345_DATARATE_0_78_HZ:
-      Serial.print  ("0.78 "); 
-      break;
-    case ADXL345_DATARATE_0_39_HZ:
-      Serial.print  ("0.39 "); 
-      break;
-    case ADXL345_DATARATE_0_20_HZ:
-      Serial.print  ("0.20 "); 
-      break;
-    case ADXL345_DATARATE_0_10_HZ:
-      Serial.print  ("0.10 "); 
-      break;
-    default:
-      Serial.print  ("???? "); 
-      break;
-  }  
-  Serial.println(" Hz");  
-}
-
-void displayRange(void)
-{
-  Serial.print  ("Range:         +/- "); 
-  
-  switch(accel.getRange())
-  {
-    case ADXL345_RANGE_16_G:
-      Serial.print  ("16 "); 
-      break;
-    case ADXL345_RANGE_8_G:
-      Serial.print  ("8 "); 
-      break;
-    case ADXL345_RANGE_4_G:
-      Serial.print  ("4 "); 
-      break;
-    case ADXL345_RANGE_2_G:
-      Serial.print  ("2 "); 
-      break;
-    default:
-      Serial.print  ("?? "); 
-      break;
-  }  
-  Serial.println(" g");  
-}
-
-#endif
-
-void handleButton(void)
+void handleButtonState(void)
 {
   int reading = digitalRead(buttonPin);
   
-  if (reading != lastButtonState) {  
+  if (reading != lastButtonState) {
     lastDebounceTime = millis();
   }
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {    
+  if ((millis() - lastDebounceTime) > debounceDelay) {
     if (reading != buttonState) {
-      buttonState = reading;      
+      buttonState = reading;
       if (buttonState == HIGH) {
         ledState = !ledState;
         logEnabled = !logEnabled;
@@ -168,29 +67,28 @@ void handleButton(void)
   lastButtonState = reading;
 }
 
-void handleDatafileState()
+void handleFileState(void)
 {
   if(logEnabled) {
-    if(dataFile == NULL) {
-      for(int i = 0; i < 100; i++) {
-        char fileName[16] = { 0 };
-        sprintf(fileName, "log%d.txt", i);
-        Serial.println(fileName);
-        if(!SD.exists(fileName)) {
-          dataFile = SD.open(fileName, FILE_WRITE);
-          if(dataFile) {
-            Serial.println("datafile opened.");
-          } else {        
-            logEnabled = false;
-            Serial.println("can not open datafile.");
-          }
-          break;
-        }
-      }      
+    if(!f.isOpen()) {
+      char buffer[11] = { 0 };
+      strcpy(buffer, "GLOG00.CSV");
+      for (int i = 0; i < 100; i++) {
+        buffer[4] = '0' + i/10;
+        buffer[5] = '0' + i%10;
+        // create if does not exist, do not open existing, write
+        if (f.open(buffer, O_CREAT | O_EXCL | O_WRITE)) break;
+      }
+      
+      if(!f.isOpen()) {
+        logEnabled = false;
+        Serial.println(F("couldnt create ")); Serial.println(buffer);
+      }
+      Serial.println(F("writing to ")); Serial.println(buffer);
     }
-  } else if(dataFile) {    
-    dataFile.close();
-    Serial.println("datafile closed.");    
+  } else if(f.isOpen()) {
+    f.close();
+    Serial.println(F("file closed."));
   }
 }
 
@@ -200,87 +98,77 @@ void setup()
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
   
+  // Initialize button state
   buttonState = digitalRead(buttonPin);
   
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
-  }
+  }  
+  Serial.println();
 
   // Initialize SD card
-  Serial.print("Initializing SD card...");
+  Serial.print(F("Initializing SD card..."));
   // make sure that the default chip select pin is set to
   // output, even if you don't use it:
   pinMode(10, OUTPUT);
-
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
+  
+  if(!card.init()) {
+    Serial.println(F("Card init. failed!"));
     return;
   }
-  Serial.println("card initialized."); 
+  if(!Fat16::init(&card)) {
+    Serial.println(F("No partition!"));
+    return;
+  }
+  Serial.println(F("card initialized."));
 
-
+  Serial.print(F("Initializing sensor ADXL345..."));
   // Initialize the sensor
   if(!accel.begin())
   {
     /* There was a problem detecting the ADXL345 ... check your connections */
-    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
-    while(1);
-  }
-  
-  // Set the range to whatever is appropriate for your project
+    Serial.println(F("No ADXL345 detected ... Check your wiring!"));
+    return;
+  }   
+  // Set the range
   accel.setRange(ADXL345_RANGE_16_G);
-  // displaySetRange(ADXL345_RANGE_8_G);
-  // displaySetRange(ADXL345_RANGE_4_G);
-  // displaySetRange(ADXL345_RANGE_2_G);
-  
+  // Set data rate
   accel.setDataRate(ADXL345_DATARATE_800_HZ);
-
-#ifdef DEBUG  
-  // Display some basic information on this sensor
-  displaySensorDetails();
+  Serial.println(F("sensor ADXL345 initialized."));
   
-  // Display additional settings (outside the scope of sensor_t)
-  displayDataRate();
-  displayRange();
-#endif
-
   Serial.println("");  
 }
 
 void loop()
 { 
-  handleButton();
-  handleDatafileState();  
+  handleButtonState();
+  handleFileState();
   
   if(logEnabled) {
     uint32_t currentMillis = millis();
     
-    // log data
+    // log data interval
     if((currentMillis - lastLogTime) >= 20) {
       lastLogTime = currentMillis;
       sensors_event_t event; 
       accel.getEvent(&event);
-         
-      String dataString = String(currentMillis) + ";" +
-        String(event.acceleration.x) + ";" + 
-        String(event.acceleration.y) + ";" + 
-        String(event.acceleration.z);
-        
-      dataFile.println(dataString);
-      dataFile.flush();
+      
+      f.writeError = false;
+      f.print(currentMillis);
+      f.print(";");
+      f.print(event.acceleration.x, 3);
+      f.print(";");
+      f.print(event.acceleration.y, 3);
+      f.print(";");
+      f.print(event.acceleration.z, 3);
+      f.println();
+
+      if (f.writeError || !f.sync()) {
+        Serial.println(F("can't write data!"));
+      }
     }
   } 
 }
-
-
-
-
-
-
-
-
 
