76c2efd84d4ab95f60b6261cb52a129a6c846c46
volthouse <lucasgaertner@arcor.de>
2015-10-20 15:53:10 +0200

    Testing.

--- WebTemperatureLogger/web.h
+++ WebTemperatureLogger/web.h
@@ -2,6 +2,15 @@
 #include <avr/pgmspace.h>
 #include "webpage.h"
 
+#define CHART_TOP			40
+#define CHART_LEFT			113
+#define CHART_RIGHT			1063
+#define CHART_BOTTOM		360
+#define CHART_X_STEP		50
+#define CHART_Y_STEP		30
+#define CHART_X_INTERVALS	20
+#define CHART_Y_INTERVALS	12
+
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
@@ -16,10 +25,27 @@ IPAddress ip(192, 168, 2, 86); // 192.168.2.86
 EthernetServer server(80);
 
 class WebManager {
+  private:
+    int m_values[5];
+    int m_temperatureLog[20];
   public:
     void begin() {
+      Rnd();
+      
       Ethernet.begin(mac, ip);
       server.begin();
+    }
+    
+    void Rnd() {
+      for(int i = 0; i < 5; i++) {
+        m_values[i] = random(10) * 10;
+        Serial.println(m_values[i]);
+      }
+      
+      for(int i = 0; i < 20; i++) {
+        m_temperatureLog[i] = random(10) * 10;;
+      }
+      
     }
     
     void dispatch() {
@@ -42,27 +68,50 @@ class WebManager {
               client.println("Connection: close");  // the connection will be closed after completion of the response
               //client.println("Refresh: 5");  // refresh the page automatically every 5 sec
               client.println();
-              //client.println("<!DOCTYPE HTML>");
-              //client.println("<html>");
               
-              //client.print("DS1821: ");
-              //client.print(Measure.temperature);
-              //client.println();
-              //client.println(RTC.getSeconds());
-              
-              //client.println("<br />");
-              
-             
-              //client.println("</html>");
               
               PGM_P page_pointer = Page1;
               
               for(;;) {
                 unsigned char b = pgm_read_byte(page_pointer++);
-                if (strncasecmp_P("%END",page_pointer,4)==0) {			  
-		  break;
-	        }
-                client.write(b);
+                if (strncasecmp_P("%END",page_pointer,4)==0) {                  
+                  client.flush();
+                  client.stop();
+                  Rnd();
+                  break;							
+                } else if (strncasecmp_P("%?", page_pointer, 2)==0) {
+                  page_pointer += 2;
+                  uint8_t idx = pgm_read_byte(page_pointer) - '0' - 1;
+
+                  char buffer[5] = { 0 };
+                  bin2asc(m_values[idx], buffer, 3);
+                  client.print(buffer);
+                  
+                  page_pointer++;
+                } else if (strncasecmp_P("%CHART", page_pointer, 6)==0) {
+                  page_pointer += 7;
+                  if (strncasecmp_P("HORIZONTAL_LINES", page_pointer, 16)==0) {
+                    chartHorizontalLines(&client);
+                    page_pointer += 16;
+                  } else if (strncasecmp_P("VERTICAL_LINES", page_pointer, 6)==0) {
+                    chartVerticalLines(&client);
+                    page_pointer += 14;
+                  } else if (strncasecmp_P("PATH", page_pointer, 4)==0) {
+                    chartPath(&client);
+                    page_pointer += 4;
+                  } else if (strncasecmp_P("POINTS", page_pointer, 6)==0) {
+                    chartCircles(&client);
+                    page_pointer += 6;
+                  } else if (strncasecmp_P("XLABELS", page_pointer, 7)==0) {
+                    chartXLabels(&client);
+                    page_pointer += 7;
+                  } else if (strncasecmp_P("YLABELS", page_pointer, 7)==0) {
+                    chartYLabels(&client);
+                    page_pointer += 7;
+                  }
+                } else {                
+                  client.write(b);
+                }
               }
               
             }
@@ -85,8 +134,135 @@ class WebManager {
           Serial.println("client disconnected");
         }
       }
-      
     }
+    
+    void chartVerticalLines(Stream* stream) {
+      //<line x1 = '113' x2 = '113' y1 = '10' y2 = '380'></line>	
+      uint16_t x = CHART_LEFT;
+      for (uint8_t i = 0; i < CHART_X_INTERVALS; i++)
+      {
+        char buffer[100] = { 0 };        
+        strcpy_P(buffer, PSTR("<line x1 = '????' x2 = '????' y1 = '10' y2 = '380'></line>"));
+        bin2asc(x, &buffer[12], 4);
+        bin2asc(x, &buffer[24], 4);
+
+        stream->println(buffer);
+
+        x += CHART_X_STEP;
+      }
+    }
+
+    void chartHorizontalLines(Stream* stream) {
+      //<line x1 = '86' x2 = '1063' y1 = '40' y2 = '40'></line>
+
+      uint16_t y = CHART_TOP;
+      for (uint8_t i = 0; i < CHART_Y_INTERVALS; i++)
+      {
+        char buffer[100] = { 0 };        
+        strcpy_P(buffer, PSTR("<line x1 = '86' x2 = '1063' y1 = '????' y2 = '????'></line>"));
+        bin2asc(y, &buffer[34], 4);
+        bin2asc(y, &buffer[46], 4);
+
+        stream->println(buffer);
+
+        y += CHART_Y_STEP;
+      }
+    }
+
+    void chartCircles(Stream* stream) {
+      //<circle cx='113' cy='40' data-value='7.2' r='5'></circle>
+
+      uint16_t x = CHART_LEFT;
+      for (uint8_t i = 0; i < CHART_X_INTERVALS; i++)
+      {        
+        char buffer[100] = { 0 };        
+        strcpy_P(buffer, PSTR("<circle cx='????' cy='????' data-value='7.2' r='5'></circle>"));
+        int y = m_temperatureLog[i] * -3 + 280;
+
+        bin2asc(x, &buffer[12], 4);
+        bin2asc(y, &buffer[22], 4);
+
+        stream->println(buffer);
+
+        x += CHART_X_STEP;
+      }
+    }
+
+    void chartXLabels(Stream* stream) {
+      //<text x='113' y='400'>1</text>
+
+	uint16_t x = CHART_LEFT;
+	for (uint8_t i = 0; i < CHART_X_INTERVALS; i++)
+	{		
+    char buffer[100] = { 0 };        
+    strcpy_P(buffer, PSTR("<text x='????' y='400'>??</text>"));
+		bin2asc(x, &buffer[9], 4);
+		bin2asc(i + 1, &buffer[23], 2);
+
+		stream->println(buffer);
+
+		x += CHART_X_STEP;
+	}
+    }
+
+    void chartYLabels(Stream* stream) {
+      //<text x='80' y='45'>80</text>
+
+	uint16_t y = CHART_TOP + 5;
+	for (uint8_t i = 0; i < CHART_Y_INTERVALS; i++)
+	{		
+    char buffer[100] = { 0 };        
+    strcpy_P(buffer, PSTR("<text x='????' y='????'>???</text>"));
+		bin2asc(80, &buffer[9], 4);
+		bin2asc(y, &buffer[18], 4);
+		bin2asc(80 - i * 10, &buffer[24], 3);
+
+		stream->println(buffer);
+
+		y += CHART_Y_STEP;
+	}
+    }
+
+    void chartPath(Stream* stream) {
+      /*<path class = 'first_set' d = '
+        M113, 360
+        
+        L113, 40
+        
+        L1063, 325
+        L1063, 360
+        Z'
+      < / path>*/
+      
+      uint16_t x = CHART_LEFT;
+   
+      char mbuffer[15] = { 0 };        
+      strcpy_P(mbuffer, PSTR("M????, ????"));
+      
+      bin2asc(x, &mbuffer[1], 4);
+      bin2asc(CHART_BOTTOM, &mbuffer[7], 4);
+      stream->println(mbuffer);
+      
+      for (uint8_t i = 0; i < CHART_X_INTERVALS; i++)
+      {        
+        char buffer[15] = { 0 };        
+        strcpy_P(buffer, PSTR("L????, ????"));
+        int y = m_temperatureLog[i] * -3 + 280;
+
+        bin2asc(x, &buffer[1], 4);
+        bin2asc(y, &buffer[7], 4);
+
+        stream->println(buffer);
+
+        x += CHART_X_STEP;
+      }
+      
+      char lbuffer[15] = { 0 };        
+      strcpy_P(lbuffer, PSTR("L????, ????"));
+      bin2asc(CHART_RIGHT, &lbuffer[1], 4);
+      bin2asc(CHART_BOTTOM, &lbuffer[7], 4);
+      stream->println(lbuffer);
+    }		
 };
 
 WebManager Web;
